dd3a0895beed23c71e61ba84b144930c5635288c
Raunak Bhagat <rabhagat31@gmail.com>
2022-04-18 00:37:12 -0700

    Finish flatten_begins

--- src/nested_asm_lang/data.rs
+++ src/nested_asm_lang/data.rs
@@ -1,1 +1,42 @@
+use crate::cpsc411;
+use crate::cpsc411::Fvar;
+use crate::cpsc411::Reg;
 
+pub struct P {
+    pub tail: Tail,
+}
+
+pub enum Tail {
+    halt {
+        triv: Triv,
+    },
+    begin {
+        effects: Vec<Effect>,
+        tail: Box<Tail>,
+    },
+}
+
+pub enum Effect {
+    set_loc_triv {
+        loc: Loc,
+        triv: Triv,
+    },
+    set_loc_binop_triv {
+        loc: Loc,
+        binop: cpsc411::Binop,
+        triv: Triv,
+    },
+    begin {
+        effects: Vec<Effect>,
+    },
+}
+
+pub enum Loc {
+    reg { reg: Reg },
+    fvar { fvar: Fvar },
+}
+
+pub enum Triv {
+    loc { loc: Loc },
+    int64 { int64: i64 },
+}
--- src/nested_asm_lang/mod.rs
+++ src/nested_asm_lang/mod.rs
@@ -1,3 +1,100 @@
 pub mod data;
 #[cfg(test)]
 mod tests;
+
+pub use self::data::*;
+use crate::para_asm_lang as target;
+
+pub struct NestedAsmLang {
+    pub p: self::P,
+}
+
+/// FlattenBegins: NestedAsmLang -> ParaAsmLang
+///
+/// ### Purpose:
+/// Flatten all nested begin expressions.
+impl From<NestedAsmLang> for target::ParaAsmLang {
+    fn from(NestedAsmLang { p }: NestedAsmLang) -> Self {
+        fn flatten_p(P { tail }: self::P) -> target::P {
+            let tail = Box::new(tail);
+            flatten_tail(tail)
+        }
+
+        fn flatten_tail(tail: Box<self::Tail>) -> target::P {
+            match *tail {
+                self::Tail::halt { triv } => {
+                    let triv = flatten_triv(triv);
+                    let halt = target::Halt { triv };
+                    target::P::begin {
+                        effects: vec![],
+                        halt,
+                    }
+                },
+                self::Tail::begin { effects, tail } => {
+                    let mut effects = flatten_effects(effects);
+                    let target::P::begin {
+                        effects: tail_effects,
+                        halt,
+                    } = flatten_tail(tail);
+
+                    effects.extend(tail_effects);
+
+                    target::P::begin { effects, halt }
+                },
+            }
+        }
+
+        fn flatten_effects(effects: Vec<self::Effect>) -> Vec<target::Effect> {
+            effects
+                .into_iter()
+                .map(flatten_effect)
+                .flatten()
+                .collect::<Vec<_>>()
+        }
+
+        fn flatten_effect(effect: self::Effect) -> Vec<target::Effect> {
+            match effect {
+                self::Effect::set_loc_triv { loc, triv } => {
+                    let loc = flatten_loc(loc);
+                    let triv = flatten_triv(triv);
+                    let effect = target::Effect::set_loc_triv { loc, triv };
+
+                    vec![effect]
+                },
+                self::Effect::set_loc_binop_triv { loc, binop, triv } => {
+                    let loc = flatten_loc(loc);
+                    let triv = flatten_triv(triv);
+                    let effect =
+                        target::Effect::set_loc_binop_triv { loc, binop, triv };
+
+                    vec![effect]
+                },
+                self::Effect::begin { effects } => effects
+                    .into_iter()
+                    .map(flatten_effect)
+                    .flatten()
+                    .collect::<Vec<_>>(),
+            }
+        }
+
+        fn flatten_triv(triv: self::Triv) -> target::Triv {
+            match triv {
+                self::Triv::loc { loc } => {
+                    let loc = flatten_loc(loc);
+                    target::Triv::loc { loc }
+                },
+                self::Triv::int64 { int64 } => target::Triv::int64 { int64 },
+            }
+        }
+
+        fn flatten_loc(loc: self::Loc) -> target::Loc {
+            match loc {
+                self::Loc::fvar { fvar } => target::Loc::fvar { fvar },
+                self::Loc::reg { reg } => target::Loc::reg { reg },
+            }
+        }
+
+        let p = flatten_p(p);
+        Self { p }
+    }
+}
